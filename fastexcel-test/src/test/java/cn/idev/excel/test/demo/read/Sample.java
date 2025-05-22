e7cdb88175ac54ef50345a2191d51644f2bb3d2b
ian zhang <ian.zhangzhe@gmail.com>
2025-05-22 20:03:36 +0800

    test: Add web input stream test (#361)
    1. Add mandatory test scope in maven dependency management
    2. Remove no meaning hg test
    3. Add web resource load test
    4. Involve mock server for remote resource loading

---
+++ fastexcel-test/src/test/java/cn/idev/excel/test/demo/read/Sample.java
@@ -0,0 +1,15 @@
+package cn.idev.excel.test.demo.read;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Sample {
+    
+    private String header;
+}
---
+++ fastexcel-test/src/test/java/cn/idev/excel/test/temp/read/WebStreamReadTest.java
@@ -0,0 +1,60 @@
+package cn.idev.excel.test.temp.read;
+
+import cn.idev.excel.EasyExcel;
+import cn.idev.excel.context.AnalysisContext;
+import cn.idev.excel.read.listener.ReadListener;
+import cn.idev.excel.test.demo.read.Sample;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+public class WebStreamReadTest {
+    
+    
+    private ClientAndServer mockServer;
+    
+    @BeforeEach
+    public void startServer() {
+        this.mockServer = ClientAndServer.startClientAndServer();
+    }
+    
+    @Test
+    public void urlLoadInputStreamTest() throws IOException {
+        mockServer.when(request().withMethod("GET").withPath("/sample.xlsx")).respond(response().withStatusCode(200)
+                .withHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+                .withBody(Files.readAllBytes(Paths.get("src/test/resources/web/io.xlsx"))));
+        URL url = new URL("http://localhost:" + mockServer.getPort() + "/sample.xlsx");
+        InputStream is = url.openStream();
+        List<String> body = new LinkedList<>();
+        EasyExcel.read(is, Sample.class, new ReadListener<Sample>() {
+            @Override
+            public void invoke(Sample data, AnalysisContext context) {
+                body.add(data.getHeader());
+            }
+            
+            @Override
+            public void doAfterAllAnalysed(AnalysisContext context) {
+            }
+        }).sheet().doRead();
+        Assertions.assertTrue(body.contains("body"));
+    }
+    
+    @AfterEach
+    public void stopServer() {
+        mockServer.stop();
+    }
+}
+
