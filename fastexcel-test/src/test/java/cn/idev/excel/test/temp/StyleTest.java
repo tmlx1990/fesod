ea9a858878f251805266138940a95e51384477ab
ian zhang <ian.zhangzhe@gmail.com>
2025-06-04 09:17:08 +0800

    fix: Fix test style (#371)
    1. remove the case no relationship with FastExcel
    2. remove no test case
    3. Provide runnable case for style

--- fastexcel-test/src/test/java/cn/idev/excel/test/temp/StyleTest.java
+++ fastexcel-test/src/test/java/cn/idev/excel/test/temp/StyleTest.java
@@ -1,32 +1,23 @@
 package cn.idev.excel.test.temp;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.List;
-
-import cn.idev.excel.EasyExcel;
-import com.alibaba.fastjson2.JSON;
-
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.crypt.Decryptor;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.BuiltinFormats;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.ExcelStyleDateFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
 
 /**
  * 临时测试
@@ -35,129 +26,66 @@ import org.slf4j.LoggerFactory;
  **/
 
 public class StyleTest {
+    
     private static final Logger LOGGER = LoggerFactory.getLogger(StyleTest.class);
-
+    
     @Test
-    public void test() {
-        List<Object> list = EasyExcel.read("D:\\test\\styleTest.xls").sheet().headRowNumber(0).doReadSync();
-        for (Object data : list) {
-            LOGGER.info("返回数据：{}", JSON.toJSONString(data));
-        }
-    }
-
-    @Test
-    public void poi() throws Exception {
-        InputStream is = new FileInputStream("D:\\test\\styleTest.xls");
-        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
-        HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(0);
-        HSSFRow hssfRow = hssfSheet.getRow(0);
-        System.out.println(hssfRow.getCell(0).getCellStyle().getDataFormatString());
-        DataFormatter formatter = new DataFormatter();
-        System.out.println(hssfRow.getCell(0).getNumericCellValue());
-        System.out.println(hssfRow.getCell(1).getNumericCellValue());
-        System.out.println(hssfRow.getCell(2).getNumericCellValue());
-        System.out.println(hssfRow.getCell(0).getCellStyle().getDataFormatString());
-        System.out.println(hssfRow.getCell(1).getCellStyle().getDataFormatString());
-        System.out.println(hssfRow.getCell(2).getCellStyle().getDataFormatString());
-
-    }
-
-    @Test
-    public void poi07() throws Exception {
-        InputStream is = new FileInputStream("D:\\test\\styleTest.xlsx");
+    public void poi07Test() throws Exception {
+        InputStream is = Files.newInputStream(Paths.get("src/test/resources/style/styleTest.xlsx"));
         Workbook workbook = WorkbookFactory.create(is); // 这种方式 Excel 2003/2007/2010 都是可以处理的
         Sheet sheet = workbook.getSheetAt(0);
         Row hssfRow = sheet.getRow(0);
-        System.out.println(hssfRow.getCell(0).getCellStyle().getDataFormatString());
-        DataFormatter formatter = new DataFormatter();
-        System.out.println(hssfRow.getCell(0).getNumericCellValue());
-        System.out.println(hssfRow.getCell(1).getNumericCellValue());
-        System.out.println(hssfRow.getCell(2).getNumericCellValue());
-        System.out.println(hssfRow.getCell(0).getCellStyle().getDataFormat());
-        System.out.println(hssfRow.getCell(1).getCellStyle().getDataFormat());
-        System.out.println(hssfRow.getCell(2).getCellStyle().getDataFormat());
-        System.out.println(hssfRow.getCell(3).getCellStyle().getDataFormat());
-        System.out.println(hssfRow.getCell(0).getCellStyle().getDataFormatString());
-        System.out.println(hssfRow.getCell(1).getCellStyle().getDataFormatString());
-        System.out.println(hssfRow.getCell(2).getCellStyle().getDataFormatString());
-        System.out.println(hssfRow.getCell(3).getCellStyle().getDataFormatString());
-        isDate(hssfRow.getCell(0));
-        isDate(hssfRow.getCell(1));
-        isDate(hssfRow.getCell(2));
-        isDate(hssfRow.getCell(3));
-
+        Assertions.assertEquals(1.0, hssfRow.getCell(0).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(1).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(2).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(3).getNumericCellValue());
+        Assertions.assertEquals(14, hssfRow.getCell(0).getCellStyle().getDataFormat());
+        Assertions.assertEquals(0, hssfRow.getCell(1).getCellStyle().getDataFormat());
+        Assertions.assertEquals(10, hssfRow.getCell(2).getCellStyle().getDataFormat());
+        Assertions.assertEquals(49, hssfRow.getCell(3).getCellStyle().getDataFormat());
+        Assertions.assertEquals("m/d/yy", hssfRow.getCell(0).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("General", hssfRow.getCell(1).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("0.00%", hssfRow.getCell(2).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("@", hssfRow.getCell(3).getCellStyle().getDataFormatString());
+        Assertions.assertTrue(isDate(hssfRow.getCell(0)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(1)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(2)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(3)));
     }
-
+    
+    
     @Test
-    public void poi0701() throws Exception {
-        InputStream is = new FileInputStream("D:\\test\\f1.xlsx");
-        Workbook workbook = WorkbookFactory.create(is);
+    public void poi03Test() throws Exception {
+        InputStream is = Files.newInputStream(Paths.get("src/test/resources/style/styleTest.xls"));
+        Workbook workbook = WorkbookFactory.create(is); // 这种方式 Excel 2003/2007/2010 都是可以处理的
         Sheet sheet = workbook.getSheetAt(0);
-        print(sheet.getRow(0).getCell(0));
-        print(sheet.getRow(1).getCell(0));
-        print(sheet.getRow(2).getCell(0));
-        print(sheet.getRow(3).getCell(0));
+        Row hssfRow = sheet.getRow(0);
+        Assertions.assertEquals(1.0, hssfRow.getCell(0).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(1).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(2).getNumericCellValue());
+        Assertions.assertEquals(1.0, hssfRow.getCell(3).getNumericCellValue());
+        Assertions.assertEquals(14, hssfRow.getCell(0).getCellStyle().getDataFormat());
+        Assertions.assertEquals(0, hssfRow.getCell(1).getCellStyle().getDataFormat());
+        Assertions.assertEquals(10, hssfRow.getCell(2).getCellStyle().getDataFormat());
+        Assertions.assertEquals(49, hssfRow.getCell(3).getCellStyle().getDataFormat());
+        Assertions.assertEquals("m/d/yy", hssfRow.getCell(0).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("General", hssfRow.getCell(1).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("0.00%", hssfRow.getCell(2).getCellStyle().getDataFormatString());
+        Assertions.assertEquals("@", hssfRow.getCell(3).getCellStyle().getDataFormatString());
+        Assertions.assertTrue(isDate(hssfRow.getCell(0)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(1)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(2)));
+        Assertions.assertFalse(isDate(hssfRow.getCell(3)));
     }
-
-    @Test
-    public void poi0702() throws Exception {
-        Workbook workbook = WorkbookFactory.create(new FileInputStream("D:\\test\\t2.xlsx"));
-        workbook = WorkbookFactory.create(new File("D:\\test\\t2.xlsx"));
-        Sheet sheet = workbook.getSheetAt(0);
-        Row row = sheet.getRow(0);
-        System.out.println(row.getCell(0).getNumericCellValue());
-    }
-
-    @Test
-    public void poi0703() throws Exception {
-        try {
-            POIFSFileSystem poifsFileSystem = new POIFSFileSystem(new FileInputStream("D:\\test\\t2.xlsx"));
-            System.out.println(poifsFileSystem.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            POIFSFileSystem poifsFileSystem = new POIFSFileSystem(new File("D:\\test\\t2.xlsx"));
-            System.out.println(poifsFileSystem.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            POIFSFileSystem poifsFileSystem = new POIFSFileSystem(new FileInputStream("D:\\test\\t222.xlsx"));
-            System.out.println(poifsFileSystem.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            POIFSFileSystem poifsFileSystem = new POIFSFileSystem(new File("D:\\test\\t222.xlsx"));
-            System.out.println(poifsFileSystem.getRoot().hasEntry(Decryptor.DEFAULT_POIFS_ENTRY));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void print(Cell cell) {
-        System.out.println(
-            DateUtil.isADateFormat(cell.getCellStyle().getDataFormat(), cell.getCellStyle().getDataFormatString()));
-        System.out.println(cell.getCellStyle().getDataFormat());
-        System.out.println(cell.getCellStyle().getDataFormatString());
-        DataFormatter f = new DataFormatter();
-        System.out.println(f.formatCellValue(cell));
-        if (cell.getCellStyle().getDataFormatString() != null) {
-
-        }
-        ExcelStyleDateFormatter ff = new ExcelStyleDateFormatter(cell.getCellStyle().getDataFormatString());
-
-    }
-
+    
+    
     @Test
     public void testFormatter() throws Exception {
         ExcelStyleDateFormatter ff = new ExcelStyleDateFormatter("yyyy年m月d日");
-
+        
         System.out.println(ff.format(new Date()));
     }
-
+    
     @Test
     public void testFormatter2() throws Exception {
         StyleData styleData = new StyleData();
@@ -170,27 +98,23 @@ public class StyleTest {
         field = styleData.getClass().getDeclaredField("byteValue3");
         LOGGER.info("field:{}", field.getType());
     }
-
+    
     @Test
     public void testFormatter3() throws Exception {
         LOGGER.info("field:{}", Byte.class == Byte.class);
     }
-
-    private void isDate(Cell cell) {
-        System.out.println(
-            DateUtil.isADateFormat(cell.getCellStyle().getDataFormat(), cell.getCellStyle().getDataFormatString()));
-        //System.out.println(HSSFDateUtil.isCellDateFormatted(cell));
-        DataFormatter f = new DataFormatter();
-        System.out.println(f.formatCellValue(cell));
-
+    
+    private boolean isDate(Cell cell) {
+        return DateUtil.isADateFormat(cell.getCellStyle().getDataFormat(), cell.getCellStyle().getDataFormatString());
+        
     }
-
+    
     @Test
     public void testBuiltinFormats() throws Exception {
         System.out.println(BuiltinFormats.getBuiltinFormat(48));
         System.out.println(BuiltinFormats.getBuiltinFormat(57));
         System.out.println(BuiltinFormats.getBuiltinFormat(28));
-
+        
     }
-
+    
 }
